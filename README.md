460d02170490fecfa68637a85af8a6ae3f68216f
utkarshg1 <gaikwadujg@gmail.com>
2024-01-23 12:38:43 +0530

    Readme updated

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# ResumeATS_pypdf
+# Smart - ATS Utkarsh Gaikwad
+This applicaton is used to check How much resume matches according to Job Description
+Used Geminin Pro Generative AI for this application
+
+---
+
+# How to run this application in VS Code
+~~~cmd
+streamlit run app.py
+~~~
+
+--- 
+
+# Home Page
+![Home Page](./UI/home_page.jpg)
+
+---
+
+# Uploading PDF and Job Description
+![Uploaded File](./UI/uploaded_file.jpg)
+
+---
+
+# Response from the Application
+![Response](./UI/Response.jpg)
+
+---
--- app.py
+++ app.py
@@ -39,25 +39,25 @@ if submit:
     if uploaded_file is not None:
         # Get the text from file
         text = input_pdf_text(uploaded_file)
+        print(text)
         #Prompt Template
-        input_prompt=f"""
-        Hey Act Like a skilled or very experienced ATS(Application Tracking System)
-        with a deep understanding of tech field,software engineering,data science ,data analyst
-        and big data engineer. Your task is to evaluate the resume based on the given job description.
-        You must consider the job market is very competitive and you should provide 
-        best assistance for improving thr resumes. 
-        Assign the percentage Matching based on Job Description and Resume
-        Also show the missing keywords with high accuracy. Please go through Entire Resume thorougly.
-        
-        Resume:{text}
-        
-        Job description:{jd}
+        input_prompt=f""" Hey Act Like a skilled or very experienced ATS(Application Tracking System)
+                          with a deep understanding of tech field,software engineering,data science ,data analyst
+                          and big data engineer. Your task is to evaluate the resume based on the given job description.
+                          You must consider the job market is very competitive and you should provide 
+                          best assistance for improving thr resumes. 
+                          Assign the percentage Matching based on Job Description and Resume
+                          Also show the missing keywords with high accuracy. Please go through Entire Resume and Job Description thorougly.
+                            
+                          Resume:{text}
+                            
+                          Job description:{jd}
 
-        I want the 3 outputs by comparing above Resume and Job Description :
-        1. Percentage match of Resume with Job Desription 
-        2. Missing Keywords in Resume in bullet points
-        3. Summary of the Resume in bullet points
-        """
+                          I want the 3 outputs by comparing above Resume and Job Description :
+                          1. Percentage match of Resume with Job Desription 
+                          2. Missing Keywords in Resume in bullet points
+                          3. Summary of the Resume in bullet points """
+        
         # Gemini response
         response = get_gemini_response(input_prompt)
         st.subheader("The Response is : ")
